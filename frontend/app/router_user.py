30dc7cdd0bafa809f38c3a5e5b605a032c35924b
oleksandr piddubnyi <sanyakent2008@gmail.com>
2025-11-27 20:01:34 +0100

    front_main

---
+++ frontend/app/router_user.py
@@ -0,0 +1,32 @@
+from fastapi import Request, APIRouter
+
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
+
+router_user = APIRouter()
+
+
+@router_user.get("/")
+async def index(request: Request):
+    context = {
+        "request": request,
+        "title": "Головна сторінка сайту",
+        "user": {}
+    }
+
+    response = templates.TemplateResponse('pages/index.html', context=context)
+
+    return response
+
+@router_user.get("/register")
+async def register(request: Request):
+    context = {
+        "request": request,
+        "title": "Головна сторінка сайту",
+        "user": {}
+    }
+
+    response = templates.TemplateResponse('pages/index.html', context=context)
+
+    return response
